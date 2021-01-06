c0d2b74b5c9fe1fdd4f509cd14c908fa13a545f6
Femi Akinyemi <santosfemakin@yahoo.com>
2021-01-06 09:50:35 +0100

    Updated App.js

--- 19_projects/19_projects_boilerplate/src/Components/App.js
+++ 19_projects/19_projects_boilerplate/src/Components/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Cat from './Cat'
+import React from "react";
+import Cat from "./Cat";
 
 export default function App() {
   return (
@@ -7,9 +7,7 @@ export default function App() {
       <h1>30 DAYS OF REACT</h1>
       <h3>Day 20</h3>
       <h2>CATS PARADISE</h2>
-      <Cat/>
-      
-     
+      <Cat />
     </div>
-  )
+  );
 }
